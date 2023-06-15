a9a15eb03a2f4e28ab90dd6db27c1ecbb58dd1d0
jsena95 <junior.senna95@live.com>
2023-06-15 20:03:56 -0300

    creating input component

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import styles from './InputField.module.css'
+import EyeClosed from './EyeClosed.svg'
+import EyeOpened from './EyeOpened.svg'
+type Props = {
+  placeholder: string;
+  value: string;
+  onChange: (newValue: string) => void;
+  isPassword?: boolean;
+}
 
-const InputField = () => {
+const InputField = ({ placeholder, value, onChange, isPassword }: Props) => {
+
+  const [showPassword, setShowPassword] = useState(false)
+  const [focused, setFocused] = useState(false)
+
   return (
-    <div>
-
+    <div className={styles.container} style={{ borderColor: focused ? '#A9DFD8' : "#fff", backgroundColor: focused ? '#fff' : "#f9f9f9" }}>
+      <input className={styles.input}
+        type={isPassword ? (showPassword ? 'text' : 'password') : 'text'}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+      />
+      {isPassword && (
+        <div className={styles.showPassword}
+          onClick={() => setShowPassword(!showPassword)}>
+          {!showPassword ? <EyeOpened color='#A9DFD8' /> : <EyeClosed color="#A9DFD8" />}
+        </div>)
+      }
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,35 @@
 import styles from '@/styles/Home.module.css'
 import InputField from '@/components/InputField'
+import { useState } from 'react'
 
 
 
 export default function Home() {
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('')
+
+
   return (
-    <>
-      <InputField />
-    </>
+    <div className={styles.container}>
+      <div className={styles.formArea}>
+        <div className={styles.inputArea}>
+          <InputField
+            placeholder='Digite seu usuário'
+            value={user}
+            onChange={setUser}
+          />
+        </div>
+        <div className={styles.inputArea}>
+          <InputField
+            placeholder='Digite sua senha'
+            value={password}
+            onChange={setPassword}
+            isPassword
+          />
+        </div>
+      </div>
+    </div>
+
   )
 }
